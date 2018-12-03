fc1701974a7c2dd2b671e2d84a74bd71b3efb291
Tirapat <mind1990@gmail.com>
2018-12-02 23:48:37 -0800

    adds Navbar, Landing  and Footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<footer className="Footer bg-dark text-white mt-5 p-4 text-center">
+		Copyright &copy; {new Date().getFullYear()} Twitbook
+		</footer>
+	);
+}
