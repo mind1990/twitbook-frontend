1962803fd01e23b5178e4c25a9dcac9e5163a357
Tirapat <mind1990@gmail.com>
2018-12-06 07:45:19 -0800

    adds mapStateToProps in Landing

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Landing extends Component {
-	render() {
-		return (
-			<div className="landing">
-    		<div className="dark-overlay landing-inner text-light">
-      		<div className="container">
-        		<div className="row">
-          		<div className="col-md-12 text-center">
-            		<h1 className="display-3 mb-4">Twitbook
-            		</h1>
-            		<p className="lead">See what’s happening in the world right now</p>
-            		<p>Join Twitbook today.</p>
-            		<hr />
-            		<Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-  		          <Link to="/login" className="btn btn-lg btn-light">Login</Link>
-          		</div>
-        		</div>
-      		</div>
-    		</div>
-  		</div>
-		);
-	}
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Twitbook</h1>
+                <p className="lead">
+                  See what’s happening in the world right now
+                </p>
+                <p>Join Twitbook today.</p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Landing;
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
